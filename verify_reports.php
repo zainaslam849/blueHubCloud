c7825795326a5e38a6666f90fbb029181b1c1492
Zain Aslam <zainaslam849@gmail.com>
2026-03-27 20:31:31 +0500

    d

--- verify_reports.php
+++ verify_reports.php
@@ -1,7 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 $app = require_once __DIR__ . '/bootstrap/app.php';
-$app->boot();
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->bootstrap();
 
 use Illuminate\Support\Facades\DB;
 
